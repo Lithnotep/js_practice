abe721953ddddb66dfc4b4ce12f301f27b12f941
Max Mitrani <mjmitrani168@gmail.com>
2020-08-15 13:38:34 -0600

    add object manipulation

--- objects_arrays.js
+++ objects_arrays.js
@@ -11,3 +11,12 @@ const team4 = [...players];
 team4[3] = 'heeee hawww';
 console.log(team4);
 console.log(team3);
+// mainly going over the basics of objects and what can be manipulated
+
+const person = {
+      name: 'Wes Bos',
+      age: 80
+    };
+
+const cap2 = Object.assign({}, person, { number: 99, age: 12 });
+console.log(cap2);
