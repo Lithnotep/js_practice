4b617ae6e2fa50fbee1a232fa854a443c7161585
Max Mitrani <mjmitrani168@gmail.com>
2020-08-15 12:24:54 -0600

    add .some practice

--- array2.js
+++ array2.js
@@ -4,11 +4,13 @@ const people = [
       { name: 'Irv', year: 1970 },
       { name: 'Lux', year: 2015 }
     ];
-
-    const comments = [
+const comments = [
       { text: 'Love this!', id: 523423 },
       { text: 'Super good', id: 823423 },
       { text: 'You are the best', id: 2039842 },
       { text: 'Ramen is my fav food ever', id: 123523 },
       { text: 'Nice Nice Nice!', id: 542328 }
     ];
+
+const isAdult = people.some(person => ((new Date()).getFullYear()) - person.year >= 19);
+console.log({isAdult});
