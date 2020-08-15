d379d03364009ec2b364a530fc4ddd5496123f67
Max Mitrani <mjmitrani168@gmail.com>
2020-08-15 13:37:23 -0600

    add basic object manipulation

--- objects_arrays.js
+++ objects_arrays.js
@@ -1,1 +1,13 @@
 const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
+
+const team = players;
+
+console.log(players, team);
+
+const team2 = players.slice();
+const team3 = [].concat(players);
+const team4 = [...players];
+
+team4[3] = 'heeee hawww';
+console.log(team4);
+console.log(team3);
